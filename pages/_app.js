fe5a0ef93f4000b844b4c027165583c0a6454e06
joaovict0r2 <joaovictorcsantos13@gmail.com>
2021-09-02 11:37:27 -0300

    initialize

---
+++ pages/_app.js
@@ -0,0 +1,51 @@
+// import '../styles/globals.css'
+// import Context from  '../context/Control'
+
+// function MyApp({ Component, pageProps }) {
+//   return(
+//   <Context>
+//   <Component {...pageProps} />
+//   </Context>
+//   )
+// }
+
+// export default MyApp
+
+
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from '../src/theme';
+import Context from  '../context/Control'
+
+export default function MyApp(props) {
+  const { Component, pageProps } = props;
+
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <React.Fragment>
+      <Head>
+        <title>My page</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Context>
+        <Component {...pageProps} />
+        </Context>
+      </ThemeProvider>
+    </React.Fragment>
+  );
+}
+
